a4bc8ec07816e9315b0c5e0367e1f977f7949cb0
@vicdauvi <victor.daudrevignier@reseau.eseo.fr>
2019-03-26 14:06:46 +0100

    first commit

---
+++ src/main/java/com/ESEO_API_REST/App.java
@@ -0,0 +1,23 @@
+package com.ESEO_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+        	SpringApplication.run(App.class, args) ;
+        	System.out.println("ET ZÉ PARTIIIIIIIIII !");
+        }
+        catch(Exception e) {
+        	System.out.println("Erreur");
+        }
+    }
+}
